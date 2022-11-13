c7eb969a61fb548fc7b4b424b6420e3f81521d7d
Zyler <508282+zylerdj@users.noreply.github.com>
2022-11-13 14:21:49 +0100

    Load Relayx, Assets from Blockchain

---
+++ template/bin/onchain-deploy.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env ts-node
+
+async function run() {
+
+  // upload all files to b://
+  // link all b:// files using onchain.sv/ln
+  
+
+}
+
+run()
+
---
+++ template/bin/onchain-init.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env ts-node
+
+async function run() {
+
+  // add .env to .gitignore
+  // add onchain.json to .gitignore
+  // create onchain.json if not exists with mnemonic seed
+  // check 
+
+}
+
+run()
+
